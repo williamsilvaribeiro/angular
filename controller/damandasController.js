d82b19048a302f8612dff75f8860f8ef60dec598
William Ribeiro <williamsilva.ribeiro@gmail.com>
2015-11-24 19:40:32 -0200

    Initial commit

---
+++ controller/damandasController.js
@@ -0,0 +1,42 @@
+var app = angular.module("demandas",['ngRoute']);
+
+app.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+        .when("/", {templateUrl: "templates/botoesPainelControle.html", controller: "damandasController"})
+        .when("/lista", {templateUrl: "templates/listagem.html", controller: "damandasController"});
+}]);
+
+
+app.controller('damandasController', function ($scope, $location) {
+
+});
+
+angular.module("demandas").controller("demandasController", function($scope){
+    $scope.botoesTopo = [
+        {nome: "Subway", pedidos: "152", cor: 'aqua'},
+        {nome: "Spoleto", pedidos: "132", cor:'green'},
+        {nome: "Conveniencia", pedidos: "98", cor:'yellow'},
+        {nome: "Drogaria", pedidos: "98", cor:'red'},
+    ];
+    $scope.menusSistema = [
+        {nome: "Gerenciar Pedidos", pagina: "/lista", cor: 'aqua'},
+        {nome: "Relatorio", pagina: "/", cor:'green'},
+        {nome: "usuarios", pagina: "/", cor:'yellow'},
+    ];
+
+    $scope.pedidos = [
+        {cod:"01", cliente:"Joao", loja: "Subway", status:"Pendente", label:"danger"},
+        {cod:"02", cliente:"Joao", loja: "Spoleto", status:"Entregue", label:"success"},
+        {cod:"03", cliente:"Joao", loja: "Subway", status:"Entregue", label:"success"},
+        {cod:"04", cliente:"Joao", loja: "Spoleto", status:"Pendente", label:"danger"},
+        {cod:"05", cliente:"Joao", loja: "Spoleto", status:"Entregue", label:"success"},
+        {cod:"06", cliente:"Joao", loja: "Subway", status:"Entregue", label:"success"},
+        {cod:"07", cliente:"Joao", loja: "Spoleto", status:"Pendente", label:"danger"},
+        {cod:"08", cliente:"Joao", loja: "Subway", status:"Entregue", label:"success"},
+        {cod:"09", cliente:"Joao", loja: "Spoleto", status:"Entregue", label:"success"},
+        {cod:"10", cliente:"Joao", loja: "Subway", status:"Pendente", label:"danger"},
+        {cod:"11", cliente:"Joao", loja: "Spoleto", status:"Entregue", label:"success"},
+        {cod:"12", cliente:"Joao", loja: "Subway", status:"Entregue", label:"success"}
+    ];
+
+});
---
+++ controller/listaController.js
@@ -0,0 +1,26 @@
+angular.module("listaTelefonica", []);
+angular.module("listaTelefonica").controller("listaControler", function ($scope) {
+    $scope.app = "Lista Telefonica";
+    $scope.contatos = [
+        {nome: "William", telefone: "9999999"},
+        {nome: "Maria", telefone: "7777777"},
+        {nome: "Teresa", telefone: "8888888"},
+    ];
+    $scope.adicionarContato = function (contato) {
+        $scope.contatos.push(contato);
+        delete $scope.contato;
+    };
+    $scope.classe = "selecionado";
+
+    $scope.apagarContatos = function (contatos) {
+        $scope.contatos = contatos.filter(function (contato) {
+            if (!contato.selecionado) return contato;
+        });
+    };
+
+    $scope.isSelecionados = function (contatos) {
+        return contatos.some(function (contato) {
+        return contato.selecionado;
+        });
+    };
+});
